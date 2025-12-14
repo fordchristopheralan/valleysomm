ed68988b5e12ea8d22e35a17ffaea13033ecf4cf
YDP-Chris <chris@yadkindatapartners.com>
2025-12-13 21:37:38 -0500

    map

--- src/components/WineryMap.tsx
+++ src/components/WineryMap.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 import L from 'leaflet';
+import type { LeafletMapEvent } from 'leaflet'; // Import the correct type
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
@@ -44,7 +45,7 @@ export default function WineryMap({ filteredWineries = wineries }: { filteredWin
       center={[36.1, -80.8]}
       zoom={10}
       style={{ height: '100%', width: '100%' }}
-      whenReady={(event) => setMap(event.target)}  // ← Correct callback
+      whenReady={(event: LeafletMapEvent) => setMap(event.target)}  // Properly typed callback
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
