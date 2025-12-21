af17c0d1acf92ed8c32c85224818900fb6b47039
fordchristopheralan <fordchristopheralan@gmail.com>
2025-12-20 23:29:17 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <head>
+            <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-12RY5VL0J6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-12RY5VL0J6');
+</script>
+</head>
       <body className="antialiased font-body">{children}</body>
     </html>
   )
